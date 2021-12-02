3cd8458879e1cd25322137191c16b6bcd7ff778a
rdogan7106 <rdogan7106@gmail.com>
2021-12-03 00:38:09 +0100

    styling content

---
+++ jquery/stylingContent.js
@@ -0,0 +1,15 @@
+$('#header').css('color','red');
+$('#list a').css('text-decoration','none')
+$('a').css('text-decoration','none')
+
+let styles ={
+    color:'green',
+    border:'1px solid blue',
+    fontSize:'40px'
+}
+$('#header').css(styles)
+$('#list a').css({
+    color:'red',
+    border:'1px solid blue',
+    fontSize:'30px'
+})
