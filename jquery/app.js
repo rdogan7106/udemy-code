99322a60003d7722b31935566ee3eb59c0598562
rdogan7106 <rdogan7106@gmail.com>
2021-12-02 22:05:36 +0100

    jquery selectors

---
+++ jquery/app.js
@@ -0,0 +1,9 @@
+$('#list');
+console.log($('#list'))
+console.log($('h3'))
+console.log($('li'))
+console.log($('.li-item'))
+console.log($('a'));
+console.log($('ul li a'));
+console.log($('#liste li a'));
+
