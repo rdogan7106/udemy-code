01751c2d4e81122be2730b4fe3b9a1a1954a86af
rdogan7106 <rdogan7106@gmail.com>
2021-12-11 15:55:46 +0100

    UI

--- courseApplication/appES5.js
+++ courseApplication/appES5.js
@@ -6,11 +6,32 @@
 function querySelector(pString){
     return document.querySelector(pString);
 }
-
+// Course Constructor
 function Course(title,instructor,image){
     this.title = title;
     this.instructor = instructor;
     this.image = image;
+}
+//UI constructor
+function UI(){
+}
+
+UI.prototype.addCourseToList = function(course){
+    const list = querySelector('#course-list');
+    let html = `
+            <tr>
+                <td><img src="./img/${course.image}"></td>
+                <td>${course.title}</td>
+                <td>${course.instructor}</td>
+                <td><a href ='#' class ='btn btn-danger btn-sm'>Delete</a></td>
+            </tr>
+        `
+        list.innerHTML += html;
+}
+UI.prototype.clearControls = function(){
+    const title = querySelector('#title').value=""
+    const instructor = querySelector('#instructor').value=""
+    const image = querySelector('#image').value=""
 }
 
 const newCourse = querySelector('#new-course');
@@ -21,8 +42,12 @@ newCourse.addEventListener('click',function(e){
     //console.log(title,instructor,image)
     //Create course Object
     const course = new Course(title,instructor,image);
-    console.log(course)
-
+    //create UI
+    const ui = new UI();
+    //add course to List
+    ui.addCourseToList(course);
+    //clear controls
+    ui.clearControls();
 
     e.preventDefault();
 })
