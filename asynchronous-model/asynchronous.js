92aae0408c4b16a7f57d33d4ff4f30ddc2bb0d26
rdogan7106 <rdogan7106@gmail.com>
2021-12-13 23:23:23 +0100

    Asynchrounus

---
+++ asynchronous-model/asynchronous.js
@@ -0,0 +1,16 @@
+const first = ()=>{
+    console.log('first');
+    second();
+}
+const second = ()=>{
+    setTimeout(()=>{
+        console.log('second');
+    },2000)
+    third()
+}
+const third = ()=>{
+    console.log('third');
+}
+first();
+
+// Asynchrounus Callback,Promise, Async/Await
